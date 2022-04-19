1f43d3b4e0f6e8f25b692e590844db8641bcdd97
David Moravec <david.moravec@cfdsupport.com>
2022-04-19 16:47:45 +0200

    making mean from mulitple rows that has the same x value now works

--- make-mean.py
+++ make-mean.py
@@ -2,9 +2,9 @@ import pandas as pd
 import sys
 import matplotlib.pyplot as plt
 
-files_to_process = [[]
 for name in sys.argv[1:]:
-    df = pd.read_csv(name, sep=' ', columns['x', 'quantity'])
-    df = df.groupby(['x']).mean()
-    df.sort_values(df.columns[0], inplace=True)
-    df.to_csv(name + "-mean.dat", sep=' ', index=False, header=False)
+    df = pd.read_csv(name, sep=' ',usecols=[0, 3], names=['x', 'quantity'])
+    mean = df.groupby(['x'])['x', 'quantity'].mean()
+    mean.reset_index(drop=True)
+    print(mean.head())
+    mean.to_csv("../" + name + "-mean.dat", sep=' ', index=False, header=False)
